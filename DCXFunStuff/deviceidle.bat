56fb2729d040a3dde2be8781cb504b37dbc73170
AnOrMaLusEr12 <176727388+AnOrmaluser12@users.noreply.github.com>
2025-03-25 21:06:49 +0700

    deviceidle.bat

--- DCXFunStuff/deviceidle.bat
+++ DCXFunStuff/deviceidle.bat
@@ -1,6 +1,8 @@
 @echo off
 chcp 65001 > nul
 setlocal enabledelayedexpansion
+::
+::
 adb shell dumpsys deviceidle sys-whitelist | findstr /R "^[ ]*[a-zA-Z0-9_.]*,[0-9]*$" > temp.txt
 for /f "tokens=1 delims=," %%A in (temp.txt) do (
     echo %%A | findstr /I "gms shell ims downloads" > nul
