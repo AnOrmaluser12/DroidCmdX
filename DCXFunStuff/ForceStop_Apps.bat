5489c4ce3f460b2d57fcaf0d7aedce1e05bd9f18
AnOrMaLusEr12 <176727388+AnOrmaluser12@users.noreply.github.com>
2025-03-25 21:07:40 +0700

    forcestop_apps.bat

--- DCXFunStuff/ForceStop_Apps.bat
+++ DCXFunStuff/ForceStop_Apps.bat
@@ -1,9 +1,9 @@
 @echo off
 for /f "tokens=2 delims=:" %%a in ('adb shell cmd package list packages') do (
 adb shell cmd activity force-stop %%a > nul 2>&1
+adb shell am kill-all --user 0 %%a
 echo Forcestop : %%a
 )
 adb shell am kill-all
 adb shell am kill-all --user 0 all
 adb shell am kill-all --user 0 current 
-adb shell am kill-all --user 0 %%a
