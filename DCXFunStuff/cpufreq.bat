67ace54ea1e237b36b3709eee69a1e4ae47e0413
AnOrMaLusEr12 <176727388+AnOrmaluser12@users.noreply.github.com>
2025-03-27 15:35:02 +0700

    Update cpufreq.bat

--- DCXFunStuff/cpufreq.bat
+++ DCXFunStuff/cpufreq.bat
@@ -1,9 +1,10 @@
 @echo off
 for /f %%a in ('adb shell cat /sys/devices/system/cpu/cpu1/cpufreq/cpuinfo_max_freq') do set MaxFreq1=%%a
 for /f %%a in ('adb shell cat /sys/devices/system/cpu/cpu4/cpufreq/cpuinfo_max_freq') do set MaxFreq4=%%a
-for /f %%b in ('powershell -command "[math]::Round(%MaxFreq1% * 0.75)"') do set LowFreq1=%%b
-for /f %%b in ('powershell -command "[math]::Round(%MaxFreq4% * 0.30)"') do set LowFreq4=%%b
-adb shell settings put global battery_saver_device_specific_constants cpufreq-i=0:%LowFreq1%/1:%LowFreq1%/2:%LowFreq1%/3:%LowFreq1%,cpufreq-n=4:%LowFreq4%/5:%LowFreq4%/6:%LowFreq4%/7:%LowFreq4%
+for /f %%b in ('powershell -command "[math]::Round(%MaxFreq1% * 0.35)"') do set LowFreq1=%%b
+for /f %%b in ('powershell -command "[math]::Round(%MaxFreq4% * 0.80)"') do set LowFreq4=%%b
+::adb shell settings put global battery_saver_device_specific_constants cpufreq-i=0:%LowFreq1%/1:%LowFreq1%/2:%LowFreq1%/3:%LowFreq1%,cpufreq-n=4:%LowFreq4%/5:%LowFreq4%/6:%LowFreq4%/7:%LowFreq4%
+adb shell settings put global battery_saver_device_specific_constants cpufreq-i=0:%LowFreq7%/1:%LowFreq7%/2:%LowFreq1%/3:%LowFreq1%,cpufreq-n=4:%LowFreq1%/5:%LowFreq1%/6:%LowFreq1%/7:%LowFreq1%
 echo.
 echo Delete =} adb shell settings delete global battery_saver_device_specific_constants
 echo.
